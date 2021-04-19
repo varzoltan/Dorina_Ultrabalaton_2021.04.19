63f018e2b117c1f13c65c8844e034b9238cc9f4e
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-04-19 17:37:01 +0200

    Add project files.

---
+++ Dorina_Ultrabalaton_2021.04.19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorina_Ultrabalaton_2021._04._19
+{
+    class Program
+    {
+        //Létrehoztuk a struktúrát
+        struct Adat
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
