997f49bf605716c0ec57b2e951b1db2d92ef80cb
varzoltan <varzoltan@tanarium.hu>
2021-04-21 14:52:39 +0200

    14:56

--- Dorina_Ultrabalaton_2021.04.19/Program.cs
+++ Dorina_Ultrabalaton_2021.04.19/Program.cs
@@ -12,7 +12,7 @@ namespace Dorina_Ultrabalaton_2021._04._19
         //Létrehoztuk a struktúrát
         struct Adat
         {
-            //Adatszerkezet létrehozása
+            //Adattagok létrehozása
             public string versenyzo;
             public int rajtszam;
             public string kategoria;
@@ -25,12 +25,32 @@ namespace Dorina_Ultrabalaton_2021._04._19
             //Példányosítjuk a struktúránkat!
             Adat[] adatok = new Adat[500];
 
-            StreamReader olvas = new StreamReader(@"E:\OneDrive - Kisvárdai SZC Móricz Zsigmond Szakgimnáziuma és Szakközépiskolája\Oktatas\Programozas\Jakab_Acs_Eszter\Erettsegi_feladatok\2019_maj_Ultrabalaton\ub2017egyeni.txt");
+            StreamReader olvas = new StreamReader(@"C:\Users\Rendszergazda\Downloads\ub2017egyeni.txt");
             //Első sor beolvasása
             string Elso_sor = olvas.ReadLine();
-
+            int n = 0;
+            while (!olvas.EndOfStream)
+            {
+                string sor = olvas.ReadLine();//Beolvassuk a teljes sort!
+                string[] darabol = sor.Split(';');
+                adatok[n].versenyzo = darabol[0];
+                adatok[n].rajtszam = int.Parse(darabol[1]);
+                adatok[n].kategoria = darabol[2];
+                adatok[n].versenyido = darabol[3];
+                adatok[n].tavszazalek = int.Parse(darabol[4]);
+                n++;
+                //n = n + 1;Ez egyenlő: n++
+            }
             //Ciklikus adatfeldolgozás
             olvas.Close();
+            Console.WriteLine("2.feladat\nBeolvasás kész!");
+
+            //2.feladat
+            //Határozza meg és írja ki a képernyőre a minta szerint, hogy hány egyéni sportoló indult el
+            //a versenyen!
+            Console.WriteLine("3. feladat: Egyéni indulól: "+ n + " fő");
+            
+            Console.ReadKey();//Vár egy billentyű leütésére!
         }
     }
 }
