06e6e2ab774501342595f7ee099b6ffdbfeccb77
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-04-23 10:06:26 +0200

    10:06

--- Dorina_Ultrabalaton_2021.04.19/Program.cs
+++ Dorina_Ultrabalaton_2021.04.19/Program.cs
@@ -25,7 +25,7 @@ namespace Dorina_Ultrabalaton_2021._04._19
             //Példányosítjuk a struktúránkat!
             Adat[] adatok = new Adat[500];
 
-            StreamReader olvas = new StreamReader(@"C:\Users\Rendszergazda\Downloads\ub2017egyeni.txt");
+            StreamReader olvas = new StreamReader(@"E:\OneDrive - Kisvárdai SZC Móricz Zsigmond Szakgimnáziuma és Szakközépiskolája\Oktatas\Programozas\Jakab_Acs_Eszter\Erettsegi_feladatok\2019_maj_Ultrabalaton\ub2017egyeni.txt");
             //Első sor beolvasása
             string Elso_sor = olvas.ReadLine();
             int n = 0;
@@ -45,11 +45,50 @@ namespace Dorina_Ultrabalaton_2021._04._19
             olvas.Close();
             Console.WriteLine("2.feladat\nBeolvasás kész!");
 
-            //2.feladat
+            //3.feladat
             //Határozza meg és írja ki a képernyőre a minta szerint, hogy hány egyéni sportoló indult el
             //a versenyen!
-            Console.WriteLine("3. feladat: Egyéni indulól: "+ n + " fő");
-            
+            Console.WriteLine("3. feladat: Egyéni indulók: "+ n + " fő");
+
+            //4.feladat
+            //for (//honnan indulunk;meddig megyünk;lépésköz)
+            //Összegzés tétele
+            int hany = 0;
+            for(int i = 0;i<n;i++)
+            {
+                if (adatok[i].kategoria == "Noi" && adatok[i].tavszazalek == 100)
+                {
+                    hany++;
+                }
+            }
+            Console.WriteLine("4. feladat: Célba érkező női sportolók: "+hany+" fő");
+
+            //5.feladat
+            //Név bekérése a felhasználótól
+            Console.Write("5. feladat: Kérem a sportoló nevét: ");
+            string nev = Console.ReadLine();
+            bool volt_e = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (nev == adatok[i].versenyzo)
+                {
+                    Console.WriteLine("\tIndult egyéniben a sportoló? Igen");
+                    volt_e = true;
+                }
+                /*if (adatok[i].tavszazalek == 100)
+                {
+                    Console.WriteLine("\tTeljesítette a teljes távot? Igen");
+                }*/
+                /*else
+                {
+                    Console.WriteLine("Indult egyéniben a sportoló? Nem");
+                }*/
+            }
+            if (volt_e == false)
+            {
+                Console.WriteLine("\tIndult egyéniben a sportoló? Nem");
+            }
+
             Console.ReadKey();//Vár egy billentyű leütésére!
         }
     }
